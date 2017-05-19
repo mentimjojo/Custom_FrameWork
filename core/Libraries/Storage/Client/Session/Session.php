291896762eb01778c06967bfdd6151d59c033436
Tim Nijborg <timnijborg@hotmail.nl>
2017-05-19 13:33:07 +0200

    Renewed session class. More friendly used now.

--- core/Libraries/Storage/Client/Session/Session.php
+++ core/Libraries/Storage/Client/Session/Session.php
@@ -40,7 +40,7 @@ class Session {
      * Unset session
      * @param $name
      */
-    public static function unsetSession($name) {
+    public static function unsetSession( string $name) {
         // unset session $name
         unset($_SESSION[$name]);
     }
@@ -55,16 +55,48 @@ class Session {
 
     /**
      * Create session
+     * @return stdClass
      */
-    public function createSession() {
-        // check if session name exists
-        if (!isset($_SESSION[$this->session_name])) {
-            // Set session
-            $_SESSION[$this->session_name] = $this->session_value;
+    public function createSession() : stdClass {
+        // if name empty
+        if(!empty($this->session_name)) {
+            // If empty value
+            if(!empty($this->session_value)) {
+                // check if session name exists
+                if (!isset($_SESSION[$this->session_name])) {
+                    // Set session
+                    $_SESSION[$this->session_name] = $this->session_value;
+                    $return = array('status' => true, 'message' => 'success');
+                } else {
+                    // Throw error
+                    $return = array('status' => false, 'message' => 'error_duplicate_name');
+                }
+            } else {
+                $return = array('status' => false, 'message' => 'error_empty_value');
+            }
         } else {
-            // Throw error
-            ErrorHandler::warning(117,'Session name duplicate found!');
+            $return = array('status' => false, 'message' => 'error_empty_name');
         }
+        // Return
+        return (object) $return;
+    }
+
+    /**
+     * Get class
+     * @param string $name
+     * @return stdClass
+     */
+    public static function get(string $name) : stdClass{
+        // Get session
+        if(isset($_SESSION[$name])){
+            // Set return
+            $return = array('status' => true, 'message' => 'success', 'session' => $_SESSION[$name]);
+        } else {
+            // Set return
+            $return = array('status' => false, 'message' => 'session_not_found');
+        }
+        // Return
+        return (object) $return;
     }
 
 }
