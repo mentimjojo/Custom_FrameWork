408f3a4bf5c7f18fc6a64663f0051e4e71dee552
mentimjojo <timnijborg@hotmail.nl>
2017-05-21 18:56:38 +0200

    Oops, now really fixed debug on startup.

--- core/Libraries/Config/System/Constants.php
+++ core/Libraries/Config/System/Constants.php
@@ -20,7 +20,7 @@ class Constants
      * DO NOT EDIT THIS. DANGER DANGER DANGER. ONLY A IDIOT WOULD EDIT THIS.
      * @var string
      */
-    const fw_version = "0.1.2.210";
+    const fw_version = "0.1.2.211";
 
     /**
      * SSL disabled by default
