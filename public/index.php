d194b2a3f73d7089fee71460f53478f8a1debf4c
Tim Nijborg <timnijborg@hotmail.nl>
2017-06-02 10:37:22 +0200

    Docu update

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Standard index.php
+ * Default Index.php
+ * The framework loads this file in. From here on you can build your complete website.
+ * DON'T DELETE THIS FILE, BECAUSE YOUR SITE WILL NOT WORK WITHOUT IT.
  */
 ?>
-
-2
