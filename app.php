2c8c3d6ac03de73f48aedeca1e01605b6812d234
mentimjojo <timnijborg@hotmail.nl>
2017-06-05 13:27:28 +0200

    Improved docu app

--- app.php
+++ app.php
@@ -1,11 +1,18 @@
 <?php
 /**
- * Get Core
+ *
+ * THIS IS THE MAIN APPLICATION FILE OF THE FRAMEWORK. DO NOT EDIT THIS UNLESS YOU KNOW WHAT YOU ARE DOING.
+ * EVERYTHING THAT HAS SOMETHING TO DO WITH YOUR SITE HAPPENS IN THE PUBLIC AND RESOURCES FOLDER.
+ * DO NOT EDIT ANY OTHER FILES EXCEPT FILES IN THOSE 2 FOLDERS.
+ *
+ * WARNING WARNING WARNING DO NOT EDIT THIS.
+ *
+ * Get Framework
  */
 require_once __DIR__ . '/framework/Framework.php';
 
 /**
- * Create new instance of core
+ * Create new instance of the framework
  */
 new Framework();
 ?>
