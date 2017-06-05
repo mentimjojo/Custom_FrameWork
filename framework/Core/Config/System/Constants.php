bfe94b015eea13a218b0224761fe7b123535e54b
mentimjojo <timnijborg@hotmail.nl>
2017-06-05 13:39:44 +0200

    Updated version

--- framework/Core/Config/System/Constants.php
+++ framework/Core/Config/System/Constants.php
@@ -22,7 +22,7 @@ class Constants
      * DO NOT EDIT THIS. DANGER DANGER DANGER. ONLY A IDIOT WOULD EDIT THIS.
      * @var string
      */
-    const fw_version = "0.1.3.304";
+    const fw_version = "0.1.3.305";
 
     /**
      * SSL disabled by default
