f2c7e61c4f5d904d6cce4da92bc90873ebccadf4
mentimjojo <timnijborg@hotmail.nl>
2017-06-08 15:17:59 +0200

    Improved errorHandler and templates. Added a traceback to easily see where the error is coming from.

--- framework/Core/ErrorHandler/ErrorHandler.php
+++ framework/Core/ErrorHandler/ErrorHandler.php
@@ -13,6 +13,44 @@ class ErrorHandler
     }
 
     /**
+     * Create trace back
+     * @param bool $glue
+     * @return string
+     */
+    private static function generateCallTrace(bool $glue = true): string
+    {
+        // create new exception
+        $e = new Exception();
+        // Explode trace
+        $trace = explode("\n", $e->getTraceAsString());
+        // reverse array to make steps line up chronologically
+        $trace = array_reverse($trace);
+        // Remove main
+        array_shift($trace);
+        // Remove call to this method
+        array_pop($trace);
+        // Count
+        $length = count($trace);
+        // Create array
+        $result = array();
+        // Foreach as count
+        for ($i = 0; $i < $length; $i++) {
+            // Add to array
+            $result[] = ($i + 1) . ')' . substr($trace[$i], strpos($trace[$i], ' ')); // replace '#someNum' with '$i)', set the right ordering
+        }
+        // Glue
+        if ($glue) {
+            // Set eol
+            $glue = PHP_EOL;
+        } else {
+            // Set br
+            $glue = '<br/>';
+        }
+        // Return
+        return "\t" . implode($glue, $result);
+    }
+
+    /**
      * Log error message
      * @param int $code
      * @param string $message
@@ -20,7 +58,7 @@ class ErrorHandler
     public static function log(int $code, string $message)
     {
         // Error message
-        $error = "Warning: " . date('H:i:s', time()) . " - Something went wrong with error code '" . $code . "' and message '" . $message . "'" . PHP_EOL;
+        $error = PHP_EOL . '[' . date('H:i:s d-m-Y', time()) . "] - Warning! Something went wrong with error code '" . $code . "' and message '" . $message . "'" . PHP_EOL . 'Trace back:' . PHP_EOL . self::generateCallTrace() . PHP_EOL;
         // Save error in file
         file_put_contents(Constants::path_resources . '/Logs/Errors/' . date('d-m-Y', time()) . '.txt', $error, FILE_APPEND);
     }
@@ -38,13 +76,15 @@ class ErrorHandler
             $template = file_get_contents(Constants::path_resources . '/Templates/Warning_Template_Debug.html');
             // Replace error code
             $template = str_replace('{Error_Code}', $code, $template);
+            // Replace error trace back
+            $template = str_replace('{Error_Trace_Back}', self::generateCallTrace(false), $template);
             // Replace error message
             $template = str_replace("{Error_Message}", $message, $template);
             // Echo warning
             echo $template;
         } else {
             // Error message
-            $error = "Warning: " . date('H:i:s', time()) . " - Something went wrong with error code '" . $code . "' and message '" . $message . "'" . PHP_EOL;
+            $error = PHP_EOL . '[' . date('H:i:s d-m-Y', time()) . "] - Warning! Something went wrong with error code '" . $code . "' and message '" . $message . "'" . PHP_EOL . 'Trace back:' . PHP_EOL . self::generateCallTrace() . PHP_EOL;
             // Save error in file
             file_put_contents(Constants::path_resources . '/Logs/Errors/' . date('d-m-Y', time()) . '.txt', $error, FILE_APPEND);
         }
@@ -63,13 +103,15 @@ class ErrorHandler
             $template = file_get_contents(Constants::path_resources . '/Templates/Error_Template_Debug.html');
             // Replace error code
             $template = str_replace('{Error_Code}', $code, $template);
+            // Replace error trace back
+            $template = str_replace('{Error_Trace_Back}', self::generateCallTrace(false), $template);
             // Replace error message
             $template = str_replace("{Error_Message}", $message, $template);
         } else {
             // Template
             $template = file_get_contents(Constants::path_resources . '/Templates/Error_Template.html');
             // Error message
-            $error = "Fatal error: " . date('H:i:s', time()) . " - Something went wrong with error code '" . $code . "' and message '" . $message . "'" . PHP_EOL;
+            $error = PHP_EOL . '[' . date('H:i:s d-m-Y', time()) . "] - Fatal error! Something went wrong with error code '" . $code . "' and message '" . $message . "'" . PHP_EOL . 'Trace back:' . PHP_EOL . self::generateCallTrace() . PHP_EOL;
             // Save error in file
             file_put_contents(Constants::path_resources . '/Logs/Errors/' . date('d-m-Y', time()) . '.txt', $error, FILE_APPEND);
         }
@@ -96,13 +138,15 @@ class ErrorHandler
             $template = str_replace("{Error_Message}", $errstr, $template);
             // Replace error line
             $template = str_replace('{Error_Line}', $errline, $template);
+            // Replace error trace back
+            $template = str_replace('{Error_Trace_Back}', self::generateCallTrace(false), $template);
             // Replace error file
             $template = str_replace("{Error_File}", $errfile, $template);
             // Echo $template
             echo $template;
         } else {
             // Error message
-            $error = "Fatal error: " . date('H:i:s', time()) . " - Something went wrong on '" . $errfile . "' on line '" . $errline . "' with error code '" . $errno . "' and message '" . $errstr . "'" . PHP_EOL;
+            $error = '[' . date('H:i:s d-m-Y', time()) . "] - Fatal error! Something went wrong on '" . $errfile . "' on line '" . $errline . "' with error code '" . $errno . "' and message '" . $errstr . "'" . PHP_EOL . 'Trace back:' . PHP_EOL . self::generateCallTrace() . PHP_EOL;
             // Save error in file
             file_put_contents(Constants::path_resources . '/Logs/Errors/' . date('d-m-Y', time()) . '.txt', $error, FILE_APPEND);
         }
