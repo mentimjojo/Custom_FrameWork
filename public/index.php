a4f650fed56b56106fd77b6ff794c7f0128787b6
Tim Nijborg <timnijborg@hotmail.nl>
2017-06-09 10:53:09 +0200

    Cleaned up index

--- public/index.php
+++ public/index.php
@@ -4,27 +4,4 @@
  * The framework loads this file in. From here on you can build your complete website.
  * DON'T DELETE THIS FILE, BECAUSE YOUR SITE WILL NOT WORK WITHOUT IT.
  */
-
-Settings::toggleSSL(true);
-Settings::toggleDebug(true);
-
-$db = new Database();
-$db->setName('test');
-$db->setEngine('PDO');
-$db->setCredentials('localhost', 'ictdev_framework', 'ictdev_framework', '#A*?fVF?kmJw');
-$db->createConnection();
-$db->setGlobal();
-
-$db = new Database();
-$db->setName('test2');
-$db->setEngine('PDO');
-$db->setCredentials('localhost', 'ictdev_framework', 'ictdev_framework', '#A*?fVF?kmJw');
-$db->createConnection();
-
-SQL::connection('test2');
-SQL::setQuery('SELECT * FROM test WHERE test_name = :name');
-SQL::setParams(array(':name' => "Tim"));
-var_dump(SQL::execute()->fetch());
-
-
 ?>
