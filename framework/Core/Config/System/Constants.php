ebf3680e657cd01052bbf94503b60f6c3efab4ce
Tim Nijborg <info@ict-focused.com>
2017-09-17 13:45:45 +0200

    Changed framework version for later usage when releasing update.

--- framework/Core/Config/System/Constants.php
+++ framework/Core/Config/System/Constants.php
@@ -22,7 +22,7 @@ class Constants
      * DO NOT EDIT THIS. DANGER DANGER DANGER. ONLY A IDIOT WOULD EDIT THIS.
      * @var string
      */
-    const fw_version = "0.1.3.319";
+    const fw_version = "0.1.4.001";
 
     /**
      * SSL disabled by default
